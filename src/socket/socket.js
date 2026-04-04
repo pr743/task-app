68a3ee74ec3b92d0b949db11ac41e64abd19556d
Prince <princemungra1340@gmail.com>
2026-04-04 18:48:02 +0530

    update page

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,8 +4,10 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_BASE_URL;
 
 const socket = io(SOCKET_URL, {
-    transports: ["websocket"],
     withCredentials: true,
+    transports: ["websocket", "polling"],
+    reconnection: true,
+
 });
 
 export default socket;
