cb66c4497e4b30b6129907d894a8923cd798ba52
Prince <princemungra1340@gmail.com>
2026-04-04 18:22:45 +0530

    update page

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 
-const SOCKET_URL = import.meta.env.VITE_API_URL;
+const SOCKET_URL = import.meta.env.VITE_BASE_URL;
 
 const socket = io(SOCKET_URL, {
     transports: ["websocket"],
